f4af1f3dde6637308199ef27b79102f86d9c2f8d
Ladislav Ondris <ladislav.ondris@seznam.cz>
2021-02-01 19:30:30 +0100

    cfg parser refactoring

--- src/core/cfg/cfg_parser.py
+++ src/core/cfg/cfg_parser.py
@@ -1,14 +1,32 @@
 import os
 import re
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Input, ZeroPadding2D, BatchNormalization, Conv2D, LeakyReLU, \
+    UpSampling2D, MaxPool2D
+from typing import Tuple, Dict, List
+from src.detection.yolov3.model import Model
+from src.utils.config import YOLO_CONFIG_FILE
+from src.detection.yolov3.model import YoloLayer
 
 
-class CfgParser():
+class CfgParser:
 
     def __init__(self):
-        self._supported_layer_types = ''
+        self._supported_layers = {
+            'net': ['learning_rate', 'batch', 'width', 'height', 'channels'],
+            'convolutional': ['filters', 'size', 'stride', 'pad', 'activation', 'batch_normalize'],
+            'upsample': ['stride'],
+            'maxpool': ['size', 'stride'],
+            'route': ['layers'],
+            'shortcut': ['from'],
+            'yolo': ['classes', 'mask', 'anchors']
+        }
+        self._custom_layers = {}
+        self.throw_error_on_unknown_attr = True
         return
 
-    def parse(self, cfg_file_path, custom_layers={}):
+    def parse(self, cfg_file_path: str, custom_layers=Dict[str, Tuple[Layer, List[str]]],
+              throw_error_on_unknown_attr=True) -> Model:
         """
         Parses cfg file and returns a model.
 
@@ -16,7 +34,7 @@ class CfgParser():
         ----------
         cfg_file_path : String
             File containing network architecture in a cfg format.
-        custom_layers : TYPE, optional
+        custom_layers : Dictionary, optional
             Custom layers in the cfg file. The key is a name of the layer in cfg file
             and the value is a subclass of tf.keras.layers.Layer. The default is {}.
 
@@ -25,12 +43,14 @@ class CfgParser():
         if not os.path.isfile(cfg_file_path) or not os.access(cfg_file_path, os.R_OK):
             raise OSError(F"File '{cfg_file_path} doesn't exist or is not accessible.'")
         self._custom_layers = custom_layers
+        self.throw_error_on_unknown_attr = throw_error_on_unknown_attr
 
         self._read_file_content(cfg_file_path)
         self._preprocess_file_content()
         self._parse_content_into_blocks()
         self._create_layers_from_blocks()
-        return self._create_model()
+        self._create_model()
+        return self.model
 
     def _read_file_content(self, cfg_file_path):
         """
@@ -73,33 +93,158 @@ class CfgParser():
                 # But allow empty blocks in cfg file without any properties. 
                 # Default values can be used in such cases.
                 if i != 0:
-                    self._blocks.append(block)
-                    block.clear()
+                    self._blocks.append(block)  # append the previous block
+                    block = {}
                 block["type"] = line[1:-1]
-            else:  # atribute of a block
+            else:  # attribute of a block
                 key, value = line.split("=")
+                if key not in self._supported_layers[block['type']]:
+                    message = F"Unsupported attribute '{key}' in block '{block['type']}' on line {i}."
+                    if self.throw_error_on_unknown_attr:
+                        raise ValueError(message)
+                    else:
+                        print("Warning: ", message)
                 block[key] = value
-
-        self._blocks.append(block)
+        self._blocks.append(block)  # append the last block
 
     def _create_layers_from_blocks(self):
-        for block in self._blocks:
+        if self._blocks[0]['type'] != 'net':
+            raise ValueError("The first block has to be the [net] block.")
+        self._setup_input_layer(self._blocks[0])
+
+        for i, block in enumerate(self._blocks[1:]):
             block_type = block['type']
-            # if not self._block_type_exists(block['type']):
-            #    raise ValueError("Unknown block type {block['type']}.")
 
             if block_type == 'convolutional':
-                self._create_layer_convolutional(block)
-            # elif block_type == 'upsample':
-            # elif block_type == 'maxpool':
-            # elif block_type == 'route':
-            # elif block_type == 'shortcut':
-            elif block_type == '':
-                pass
+                self._create_layer_convolutional(block, i)
+            elif block_type == 'upsample':
+                self._create_layer_upsample(block, i)
+            elif block_type == 'maxpool':
+                self._create_layer_maxpool(block, i)
+            elif block_type == 'route':
+                self._create_layer_route(block, i)
+            elif block_type == 'shortcut':
+                self._create_layer_shortcut(block, i)
+            elif block_type == 'yolo':
+                self._create_layer_yolo(block, i)
+            else:
+                raise ValueError(F"Unknown block type {block['type']}.")
 
-    def _create_layer_convolutional(self, block):
-        pass
+            """ Save the outputs of each layer """
+            self.outputs.append(self.inputs)
+
+    def _is_block_type_supported(self, block_type: str) -> bool:
+        if block_type in self._supported_layers:
+            return True
+        if block_type in self._custom_layers:
+            return True
+        if block_type == 'net':
+            return True
+        return False
+
+    def _setup_input_layer(self, net_block):
+        input_shape = (int(net_block['width']), int(net_block['height']), int(net_block['channels']))
+        batch_size = int(net_block['batch'])
+        learning_rate = float(net_block['learning_rate'])
+
+        self.model = Model()
+        self.model.input_shape = input_shape
+        self.model.batch_size = batch_size
+        self.model.learning_rate = learning_rate
+        self.model.anchors = []
+
+        self.inputs = self.input_layer = Input(shape=input_shape, batch_size=batch_size)
+        self.outputs = []
+
+    def _create_layer_convolutional(self, block, i: int):
+        filters = int(block['filters'])
+        kernel_size = int(block['size'])
+        stride = int(block['stride'])
+        padding = block['pad']
+        activation = block['activation']
+
+        try:
+            batch_normalize = int(block["batch_normalize"])
+            bias = False
+        except KeyError:
+            batch_normalize = 0
+            bias = True
+
+        if padding:
+            pad = 'same'
+        else:
+            pad = 'valid'
+
+        if stride > 1:
+            self.inputs = ZeroPadding2D(((1, 0), (1, 0)))(self.inputs)
+
+        self.inputs = Conv2D(filters, kernel_size, strides=(stride, stride),
+                             padding=pad, activation=None, use_bias=bias,
+                             name=F"conv_{i}")(self.inputs)
+
+        if batch_normalize:
+            self.inputs = BatchNormalization(name=F"bnorm_{i}")(self.inputs)
+
+        if activation == 'leaky':
+            self.inputs = LeakyReLU(alpha=.1, name=F"leaky_{i}")(self.inputs)
+        elif activation != 'linear':
+            raise Exception('Unknown activation function')
+
+    def _create_layer_upsample(self, block, i):
+        stride = int(block['stride'])
+        self.inputs = UpSampling2D(stride, name=F"upsample_{i}")(self.inputs)
+
+    def _create_layer_maxpool(self, block, i):
+        size = int(block['size'])
+        stride = int(block['stride'])
+
+        self.inputs = MaxPool2D(pool_size=(size, size), strides=(stride, stride), name=F"maxpool_{i}")(self.inputs)
+
+    def _create_layer_route(self, block, i):
+        layers = block['layers'].split(',')
+        layers = [int(layer) for layer in layers]
+
+        if layers[0] > 0:
+            layers[0] = layers[0] - i
+
+        if len(layers) == 1:
+            self.inputs = self.outputs[i + layers[0]]
+        elif len(layers) == 2:
+            if layers[1] > 0:
+                layers[1] = layers[1] - i
+
+            # reorganize the previous layer
+            target_shape = self.outputs[i + layers[1]].shape
+            self.outputs[i + layers[0]] = tf.reshape(self.outputs[i + layers[0]],
+                                                     [target_shape[0], target_shape[1], target_shape[2], -1])
+            self.inputs = tf.concat([self.outputs[i + layers[0]], self.outputs[i + layers[1]]], axis=-1)
+        else:
+            raise Exception('Invalid layers')
+
+    def _create_layer_shortcut(self, block, i):
+        from_layer = int(block["from"])
+        self.inputs = self.outputs[i - 1] + self.outputs[i + from_layer]
+
+    def _create_layer_yolo(self, block, i):
+        num_classes = int(block['classes'])
+        mask = block['mask'].split(',')
+        mask = [int(m) for m in mask]
+        anchors = block['anchors'].split(',')
+        anchors = [float(a) for a in anchors]
+        anchors = [(anchors[i], anchors[i + 1])
+                   for i in range(0, len(anchors), 2)]
+        anchors = [anchors[m] for m in mask]
+        prediction = YoloLayer(anchors, num_classes, self.input_layer.shape, name=F"yolo_{i}")(self.inputs)
+
+        self.model.anchors.append(anchors)
+        self.model.yolo_out_preds.append(prediction)
+        self.model.yolo_output_shapes.append(prediction.shape)
 
     def _create_model(self):
+        self.model.tf_model = tf.keras.Model(self.input_layer, self.model.yolo_out_preds)
 
-        return
+
+if __name__ == '__main__':
+    parser = CfgParser()
+    model = parser.parse(YOLO_CONFIG_FILE, throw_error_on_unknown_attr=False)
+    model.tf_model.summary()
--- src/detection/yolov3/model.py
+++ src/detection/yolov3/model.py
@@ -1,8 +1,6 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, BatchNormalization, \
-    LeakyReLU, ZeroPadding2D, UpSampling2D, MaxPool2D
-from src.utils.config import YOLO_CONFIG_FILE
+from tensorflow.keras.layers import Input
+from src.core.cfg.cfg_parser import CfgParser
 
 
 class YoloLayer(tf.keras.layers.Layer):
@@ -39,7 +37,7 @@ class YoloLayer(tf.keras.layers.Layer):
         batch_size = inputs_shape[0]
         grid_size_y = inputs_shape[1]
         grid_size_x = inputs_shape[2]
-        reshaped_inputs = tf.reshape(inputs, [batch_size, grid_size_y, grid_size_x, \
+        reshaped_inputs = tf.reshape(inputs, [batch_size, grid_size_y, grid_size_x,
                                               self.n_anchors, 5 + self.n_classes])
 
         # extract information
@@ -57,7 +55,7 @@ class YoloLayer(tf.keras.layers.Layer):
 
         # for example: 416 x 416 pixel images, 13 x 13 tiles
         # 416 // 13 = 32
-        stride = (self.input_layer_shape[1] // grid_size_y, \
+        stride = (self.input_layer_shape[1] // grid_size_y,
                   self.input_layer_shape[2] // grid_size_x)
 
         # get dimensions in pixels instead of grid boxes by multiplying with stride
@@ -77,190 +75,10 @@ class Model:
         self.tf_model = None
         self.learning_rate = 1e-3
         self.anchors = []
-        return
-
-    @property
-    def yolo_output_shapes(self):
-        return self._yolo_output_shapes
-
-    @yolo_output_shapes.setter
-    def yolo_output_shapes(self, value):
-        self._yolo_output_shapes = value
-
-    @property
-    def tf_model(self):
-        return self._tf_model
-
-    @tf_model.setter
-    def tf_model(self, value):
-        self._tf_model = value
+        self.yolo_out_preds = []
+        self.yolo_output_shapes = []
 
     @classmethod
-    def from_cfg(cls, cfg_file_path):
-        blocks = Model.parse_cfg(cfg_file_path)
-        net_info_block = blocks[0]
-        model = Model.create_model_from_blocks(blocks)
-        model.learning_rate = float(net_info_block['learning_rate'])
-        return model
-
-    @classmethod
-    def parse_cfg(cls, cfgfile):
-        lines = Model.get_preprocessed_cfg(cfgfile)
-
-        block = {}
-        blocks = []
-
-        for line in lines:
-            if line[0] == "[":  # This marks the start of a new block
-                if len(block) != 0:  # If block is not empty, implies it is storing values of previous block.
-                    blocks.append(block)  # add it the blocks list
-                    block = {}  # re-init the block
-                block["type"] = line[1:-1].rstrip()
-            else:
-                key, value = line.split("=")
-                block[key.rstrip()] = value.lstrip()
-        blocks.append(block)
-
-        return blocks
-
-    @classmethod
-    def get_preprocessed_cfg(cls, cfgfile):
-        with open(cfgfile, 'r') as file:
-            lines = file.read().split('\n')  # store the lines in a list
-        lines = [x for x in lines if len(x) > 0]  # get rid of the empty lines
-        lines = [x for x in lines if x[0] != '#']  # get rid of comments
-        lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces
-        return lines
-
-    @classmethod
-    def create_model_from_blocks(cls, blocks):
-        outputs = []
-        filters = []
-        yolo_out_preds = []
-        yolo_out_shapes = []
-        anchors_all = []
-
-        net_info = blocks[0]
-        input_shape = (int(net_info['width']), int(net_info['height']), int(net_info['channels']))
-        batch_size = int(net_info['batch'])
-
-        inputs = input_layer = Input(shape=input_shape, batch_size=batch_size)
-        print("Input shape", inputs.shape)
-
-        for i, block in enumerate(blocks[1:]):
-            block_type = block['type']
-            if block_type == 'convolutional':
-                filters = int(block['filters'])
-                kernel_size = int(block['size'])
-                stride = int(block['stride'])
-                padding = block['pad']
-                activation = block['activation']
-
-                try:
-                    batch_normalize = int(block["batch_normalize"])
-                    bias = False
-                except:
-                    batch_normalize = 0
-                    bias = True
-
-                if padding:
-                    pad = 'same'
-                else:
-                    pad = 'valid'
-
-                if stride > 1:
-                    inputs = ZeroPadding2D(((1, 0), (1, 0)))(inputs)
-
-                inputs = Conv2D(filters, kernel_size, strides=(stride, stride),
-                                padding=pad, activation=None, use_bias=bias,
-                                name=F"conv_{i}")(inputs)
-
-                if batch_normalize:
-                    inputs = BatchNormalization(name=F"bnorm_{i}")(inputs)
-
-                if activation == 'leaky':
-                    inputs = LeakyReLU(alpha=.1, name=F"leaky_{i}")(inputs)
-                elif activation == 'linear':
-                    # do nothing..
-                    pass
-                else:
-                    raise Exception('Unknown activation function')
-
-            elif block_type == 'upsample':
-                stride = int(block['stride'])
-                inputs = UpSampling2D(stride)(inputs)
-
-            elif block_type == 'maxpool':
-                size = int(block['size'])
-                stride = int(block['stride'])
-
-                inputs = MaxPool2D(pool_size=(size, size), strides=(stride, stride))(inputs)
-
-            elif block_type == 'route':
-                layers = block['layers'].split(',')
-                layers = [int(l) for l in layers]
-
-                if layers[0] > 0:
-                    layers[0] = layers[0] - i
-
-                if len(layers) == 1:
-                    # filters = output_filters[i + layers[0]]
-                    inputs = outputs[i + layers[0]]
-                elif len(layers) == 2:
-                    if layers[1] > 0:
-                        layers[1] = layers[1] - i
-
-                    # reorganize the previous layer 
-                    target_shape = outputs[i + layers[1]].shape
-                    outputs[i + layers[0]] = tf.reshape(outputs[i + layers[0]],
-                                                        [target_shape[0], target_shape[1], target_shape[2], -1])
-                    # filters = output_filters[i + layers[0]] + output_filters[i + layers[1]]
-                    inputs = tf.concat([outputs[i + layers[0]], outputs[i + layers[1]]], axis=-1)
-                else:
-                    raise Exception('Invalid layers')
-
-            elif block_type == 'shortcut':
-                from_layer = int(block["from"])
-                inputs = outputs[i - 1] + outputs[i + from_layer]
-
-            elif block_type == 'yolo':
-                num_classes = int(block['classes'])
-                mask = block['mask'].split(',')
-                mask = [int(m) for m in mask]
-                anchors = block['anchors'].split(',')
-                anchors = [float(a) for a in anchors]
-                anchors = [(anchors[i], anchors[i + 1])
-                           for i in range(0, len(anchors), 2)]
-                anchors = [anchors[m] for m in mask]
-                anchors_all.append(anchors)
-
-                prediction = YoloLayer(anchors, num_classes,
-                                       input_layer.shape, name=F"yolo_{i}")(inputs)
-
-                yolo_out_preds.append(prediction)
-                yolo_out_shapes.append(prediction.shape)
-            else:
-                raise ValueError(F"Unexpected block type while parsing YOLO cfg file: {block_type}")
-
-            outputs.append(inputs)
-            # output_filters.append(filters)
-            print(outputs[-1])
-
-        tf_model = tf.keras.Model(input_layer, yolo_out_preds)
-
-        model = Model()
-        model.tf_model = tf_model
-        model.input_shape = input_shape
-        model.yolo_output_shapes = yolo_out_shapes
-        model.anchors = anchors_all
-        model.batch_size = batch_size
-        return model
-
-
-def test_model_architecture():
-    model = Model.from_cfg(YOLO_CONFIG_FILE)
-    print(model.tf_model.summary())
-
-
-if __name__ == '__main__':
-    test_model_architecture()
+    def from_cfg(cls, file):
+        parser = CfgParser()
+        return parser.parse(file)
