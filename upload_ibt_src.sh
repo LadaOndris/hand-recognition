905c29cf16d3592f78fe7e71962fccf9473531e5
Ladislav Ondris <ladislav.ondris@seznam.cz>
2021-03-25 17:02:58 +0100

    uploading only source files to metacentrum

--- upload_ibt_src.sh
+++ upload_ibt_src.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-scp -r ~/projects/IBT/src/ ladislav_ondris@storage-brno6.metacentrum.cz:/home/ladislav_ondris/IBT/
+# Enable recursive **
+shopt -s globstar
+
+scp -r ~/projects/IBT/src/**/*.py ladislav_ondris@storage-brno6.metacentrum.cz:/home/ladislav_ondris/IBT/
 
