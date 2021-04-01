1f1cb46abcf86980e1b62d5cdee1082893d162c9
Ladislav Ondris <ladislav.ondris@seznam.cz>
2021-04-01 21:17:06 +0200

    yolo training plot

--- src/utils/analyse_logdir.py
+++ src/utils/analyse_logdir.py
@@ -1,5 +1,3 @@
-from packaging import version
-
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
@@ -9,7 +7,7 @@ from src.utils.paths import DOCS_DIR
 from src.utils.plots import save_show_fig
 
 
-def load_stats(experiment_id: str = "LCXAHo9bQZqEzqXZJ9cAUA"):
+def load_stats(experiment_id: str):
     experiment = tb.data.experimental.ExperimentFromDev(experiment_id)
     df = experiment.get_scalars()
     return df
@@ -28,13 +26,21 @@ def plot_epoch_loss(df: pd.DataFrame, fig_location: str = None,
     g.set_ylabel('Loss')
     g.set_xlabel('Epoch')
     g.set(yscale='log')
+    g.set_ylim(top=0.03)
+    g.set_yticks([0.03, 0.02, 0.01, 0.005, 0.0025])
+    g.set_yticklabels([0.03, 0.02, 0.01, 0.005, 0.0025])
     fig.tight_layout()
     ax.legend(title='')
     save_show_fig(fig, fig_location, show_figure)
 
 
 if __name__ == "__main__":
+    """
+    The logdir has to be uploaded to tensorboard dev:
+    tensorboard dev upload --logdir ./logs/20210315-143811/
+    See: https://www.tensorflow.org/tensorboard/dataframe_api
+    """
     location = DOCS_DIR.joinpath('images/tiny_yolov3_epoch_loss.png')
-    df = load_stats()
+    df = load_stats(experiment_id='yxDzYYTETMO0I0GZb4Ab2A')
     plot_epoch_loss(df, fig_location=location, show_figure=True)
     pass
