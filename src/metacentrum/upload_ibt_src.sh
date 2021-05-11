01023692708baffd1399615570f17af904beffff
Ladislav Ondris <ladislav.ondris@seznam.cz>
2021-05-11 16:38:11 +0200

    text of the thesis

---
+++ src/metacentrum/upload_ibt_src.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Enable recursive **
+#shopt -s globstar
+
+scp -r ~/projects/IBT/src/ ladislav_ondris@storage-brno6.metacentrum.cz:/home/ladislav_ondris/IBT/
+
--- upload_ibt_src.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Enable recursive **
-#shopt -s globstar
-
-scp -r ~/projects/IBT/src/ ladislav_ondris@storage-brno6.metacentrum.cz:/home/ladislav_ondris/IBT/
-
